2baae24fdfe0937c356765bdcb7ba3861c0a4fe0
민경환 <ber01@naver.com>
2021-10-07 15:14:12 +0900

    chore(): init

---
+++ src/app.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post, Type } from '@nestjs/common'
+import { AppService } from './app.service'
+
+type ResponseType = {
+  type: string
+  data: string
+}
+
+type PostBodyType = {
+  data: string
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  public getHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Get('/get')
+  public getRequest(): ResponseType {
+    return { type: 'GET', data: 'getResponse' }
+  }
+
+  @Post('/post')
+  public postRequest(@Body() body: PostBodyType): ResponseType {
+    return { type: 'POST', data: body.data }
+  }
+}
